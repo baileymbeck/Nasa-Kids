79cac2b5fd16d9ee59f75acc66feac70617bc596
Kristen Stroup <kristenwhitson@gmail.com>
2019-11-07 17:59:56 -0600

    initial commit for trivia game

---
+++ client/components/TriviaGame/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+function Main() {
+    return (
+        <div>
+             <Header/>
+             <Footer/>
+            
+         </div>
+    )
+}
+export default Main;
+
---
+++ client/components/TriviaGame/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function End() {
+    return (
+        <div>End</div>
+
+
+
+    )
+}
+export default End
