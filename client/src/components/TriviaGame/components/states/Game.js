bd1fb0b8638c82dac3532e4724a5b30f8bdb052c
Kristen Stroup <kristenwhitson@gmail.com>
2019-11-08 10:52:06 -0600

    pull changes

--- client/src/components/TriviaGame/components/states/Game.js
+++ client/src/components/TriviaGame/components/states/Game.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 
-const questions = {
+const questions = [
+{
             question: "What was the first full length CGI movie?",
             answers: ["A Bug's Life", "Monsters Inc.", "Toy Story", "The Lion King"],
             correctAnswer: "Toy Story",
@@ -41,7 +42,8 @@ const questions = {
             answers: ["Mr.Zhou", "Mr.Driggers", "Mr.Belding", "Mr.Page"],
             correctAnswer: "Mr.Belding",
             image: "assets/images/belding.gif"
-          };
+          }
+        ]
                
 
 function Game ({nextState, answerQuestion}) {
