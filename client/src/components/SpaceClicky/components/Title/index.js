49cd99ea0a57463fdcdf493c4a0ad864a2d97d25
Angelica Zamer <angelica.zamer@gmail.com>
2019-11-11 14:01:05 -0600

    starting clicky medium

---
+++ client/src/components/SpaceClicky/components/Title/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Title(props) {
+  return (
+    <div className="header">
+        <div className="planet-title">{props.children} Planet Shuffle!</div>
+        <div className="scores">
+        Score: {props.score} Highscore: {props.highscore}
+        </div>
+        <div className="instructions">{props.rightWrong}</div>
+    </div>
+  );
+}
+
+export default Title;
