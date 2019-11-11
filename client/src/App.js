169ab0fcb19272ccb2b8e5d9880d43022c61815e
Angelica Zamer <angelica.zamer@gmail.com>
2019-11-11 10:25:10 -0600

    images look correct

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import TriviaGame from "./components/TriviaGame";
-import Nav from "./components/Nav";
-import Home from "./pages/Home/Home"
+import ClickyGame from "./components/ClickyGame";
+// import Nav from "./components/Nav";
+// import Home from "./pages/Home/Home"
 
 const style = {
   app: {
@@ -17,12 +18,12 @@ function App() {
   return (
     <Router>
       <div style={style.app}>
-        <Nav/>
+        {/* <Nav/> */}
           <Switch>
-            <Route exact path="/" component={Home} />
+            {/* <Route exact path="/" component={Home} /> */}
             {/* <Route exact path="/login" component={} /> */}
             {/* <Route exact path="/profile" component={} /> */}
-            {/* <Route exact path="/clickygame" component={Clicky Game} /> */}
+            <Route exact path="/clickygame" component={ClickyGame} />
           <Route exact path="/trivia" component={TriviaGame} />
           </Switch>
         {/* <Footer/> */}
--- client/src/components/ClickyGame/index.js
+++ client/src/components/ClickyGame/index.js
@@ -62,6 +62,7 @@ class App extends Component {
       clicked: []
     });
     this.handleShuffle();
+    
   };
 
   handleShuffle = () => {
