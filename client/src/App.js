39611cd6242337f9da5961f314ef193f1a103ee3
Stardust <52759497+EHammons@users.noreply.github.com>
2019-11-11 13:22:31 -0600

    navigation and buttons are working

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import TriviaGame from "./components/TriviaGame";
 import Nav from "./components/Nav";
-import Home from "./pages/Home/Home"
+import Home from "./pages/Home/Home";
+import GameHome from "./pages/GameHome/GameHome"
 
 const style = {
   app: {
@@ -20,6 +21,7 @@ function App() {
         <Nav/>
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/gamehome" component={GameHome} />
             {/* <Route exact path="/login" component={} /> */}
             {/* <Route exact path="/profile" component={} /> */}
             {/* <Route exact path="/clickygame" component={Clicky Game} /> */}
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -35,8 +35,8 @@ class Nav extends Component {
         <Link className="navbar-brand" to="/">
             <img className="settings" src={Settings} />
         </Link>
-        <div className="title" style={{fontFamily: {Font1}}}>Welcome<br />
-        (Child's Name)</div>
+        <div className="title" style={{fontFamily: {Font1}}}>WELCOME<br />
+        Camila</div>
         <div>
             <img className="logo" src={Logo}/>
         </div>
