51c12ce36e293e1f543c6c9ff1f887b5ac00c95d
Stardust <52759497+EHammons@users.noreply.github.com>
2019-11-16 16:38:39 -0600

    fixed trivia pointer and working on hangman

--- client/src/components/Hangman/index.js
+++ client/src/components/Hangman/index.js
@@ -5,41 +5,96 @@ import Input from "./components/Input";
 class Hangman extends Component {
     state = {
         puzzle: words[Math.floor(Math.random() * words.length)],
-        guessedLetters: []        
-        }
+        lettersInPuzzle: [],
+        correct: [],
+        guessedLetters: [],
+        incorrect: "",
+        wordView: "",
+        guessesLeft: 8
+    }
+
+    componentDidMount() {
+        this.setUpGame();
+        console.log(this.state)
+    }
+
+    setUpGame() {
+        this.state.lettersInPuzzle = this.state.puzzle.split("");
+        this.rebuildProgress();
+    }
+
+    rebuildProgress() {
+        var wordprog = ""
+        for (var i=0; i<this.state.lettersInPuzzle.length; i++) {
+            if (this.state.correct.indexOf(this.state.lettersInPuzzle[i]) !==-1) {
+                wordprog += this.state.lettersInPuzzle[i];
+            }
+            else {
+                wordprog += "_ ";
+            }
+        };
+        console.log(wordprog);
+        this.setState({wordView: wordprog})
+    }
 
     handleGuessedLetter(letter) {
-        if(this.state.guessedLetters.includes(letter)) {
-            alert("You already guessed this letter")
+        if (this.state.guessesLeft === 0) {
+            this.setUpGame()
         } else {
-            let guessedLettersClone = [this.state.guessedLetters]
-            guessedLettersClone.push(letter)
-            this.setState({ guessedLetters: guessedLettersClone})
+            this.updateGuesses(letter);
+            this.updateMatchedLetters(letter);
+            this.rebuildProgress();
         }
     }
 
-    getIncorrectGuesses() {
-        const output = this.state.guessedLetters.filter(letter => this.state.puzzle.includes(letter) === false)
-        return output
+    updateGuesses(letter) {
+        if ((this.state.guessedLetters.indexOf(letter) === -1) && (this.state.lettersInPuzzle.indexOf(letter) === -1)) {
+            this.state.incorrect += letter;
+            this.state.guessesLeft--;
+        }
     }
 
-    getCorrectGuesses() {
-        const output = this.state.guessedLetters.filter(letter => this.state.puzzle.includes(letter))
-        return output
-    }
-
-    displayCurrentProgress() {
-        let output = ""
-        for (let index in this.state.puzzle){
-            let letter = this.state.puzzle.charAt(index)
-            if (this.state.guessedLetters.includes(letter)){
-                output += `${letter}`
-            } else {
-                output += '_ '
+    updateMatchedLetters(letter) {
+        for (var i = 0; i < this.state.lettersInPuzzle.length; i++) {
+            if ((letter === this.state.lettersInPuzzle[i]) && (this.state.correct.indexOf(letter) === -1)) {
+                this.state.correct += letter;
             }
         }
-        return output;
     }
+
+
+    // handleGuessedLetter(letter) {
+    //     if(this.state.guessedLetters.includes(letter)) {
+    //         alert("You already guessed this letter")
+    //     } else {
+    //         let guessedLettersClone = [this.state.guessedLetters]
+    //         guessedLettersClone.push(letter)
+    //         this.setState({ guessedLetters: guessedLettersClone})
+    //     }
+    // }
+
+    // getIncorrectGuesses() {
+    //     const incorrect = this.state.guessedLetters.filter(letter => this.state.puzzle.includes(letter) === false)
+    //     return incorrect
+    // }
+
+    // getCorrectGuesses() {
+    //     const correct = this.state.guessedLetters.filter(letter => this.state.puzzle.includes(letter))
+    //     return correct
+    // }
+
+    // displayCurrentProgress() {
+    //     let progress = ""
+    //     for (let index in this.state.puzzle){
+    //         let letter = this.state.puzzle.charAt(index)
+    //         if (this.state.guessedLetters.includes(letter)){
+    //             progress += `${letter}`
+    //         } else {
+    //             progress += '_ ';
+    //         }
+    //     }
+    //     return progress;
+    // }
 
     render() {
         return (
@@ -51,8 +106,8 @@ class Hangman extends Component {
             You have 8 tries to guess the word before Earth crashes into the sun. Go!
             </h3>
             {this.state.puzzle}
-            <p>Incorrect Guesses: {this.getIncorrectGuesses()} </p>
-            <p>Current Progress: {this.displayCurrentProgress()} </p>
+            <p>Incorrect Guesses: {this.state.incorrect} </p>
+            <p>Current Progress: {this.state.wordView} </p>
             <Input handleGuessedLetter={this.handleGuessedLetter.bind(this)} />
             </div>
         );
