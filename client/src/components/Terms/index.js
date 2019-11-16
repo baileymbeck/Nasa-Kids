62ced9512eaa2655a968354da2e13366a96bac0f
Stardust <52759497+EHammons@users.noreply.github.com>
2019-11-15 20:22:02 -0600

    adding images for glossary and hangman game

--- client/src/components/Terms/index.js
+++ client/src/components/Terms/index.js
@@ -1,24 +1,21 @@
 import React from "react";
-import { Col, Row, Container } from "../../components/Grid";
+import { Col, Row } from "../../components/Grid";
 import "./style.css";
 
 function Terms(props) {
   return (
-          <Row className="row">
-              <Col size="md-2">
-                  <div className="word" value={props.id}>
-                      {props.term}
-                    </div>
-              </Col>
-              <Col size="md-7">
-                  <p className="defined">
-                      {props.definition}
-                  </p>
-              </Col>
-              <Col size="md-3">
-
-              </Col>
-          </Row>
+    <Row>
+        <Col size="md-2">
+            <div className="word" value={props.id}>
+                {props.term}
+            </div>
+        </Col>
+        <Col size="md-10">
+            <p className="defined">
+                {props.definition}
+            </p>
+        </Col>
+    </Row>
   );
 }
 
--- client/src/pages/Glossary/Glossary.js
+++ client/src/pages/Glossary/Glossary.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
 import { Col, Row, Container } from "../../components/Grid";
 import "./style.css";
-import Terms from "../../components/Terms"
-import glossary from "./glossary.json"
+import Terms from "../../components/Terms";
+import glossary from "./glossary.json";
+import Atom from "./atom.png";
+import Comet from "./comet.png";
+import Empty from "./empty.png"
+
 
 class Glossary extends Component {
     state={
@@ -12,14 +16,30 @@ class Glossary extends Component {
     render() {
         return (
             <Container fluid>
-                <div>{this.state.glossary.map(glossary => (
-                    <Terms 
-                        id={glossary.id}
-                        term={glossary.term}
-                        definition={glossary.definition}
-                />
-                ))}
-                </div>
+                <Row className="row">
+                    <Col size="md-9">
+                        <div>{this.state.glossary.map(glossary => (
+                            <Terms 
+                                id={glossary.id}
+                                term={glossary.term}
+                                definition={glossary.definition}
+                        />
+                        ))}
+                        </div>
+                    </Col>
+                    <Col size="md-3">
+                        <Row>
+                            <Col size="md-6"></Col>
+                            <Col size="md-6">
+                            <img className="glossimg" src={Atom}/>
+                            </Col>
+                        </Row>
+                        <Row><Col size="md-12"><img src={Empty} /></Col></Row>
+                        <Row>
+                            <img className="glossimg" src={Comet}/>
+                        </Row>
+                    </Col>
+                </Row>
             </Container>
         );
     }    
