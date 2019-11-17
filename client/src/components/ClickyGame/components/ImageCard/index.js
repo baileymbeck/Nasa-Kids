661d58a88fd4fe43e366b473e75568de12d0d5b4
Kristen Stroup <kristenwhitson@gmail.com>
2019-11-17 15:32:03 -0600

    test process.env.PUBLIC_URL

--- client/src/components/ClickyGame/components/ImageCard/index.js
+++ client/src/components/ClickyGame/components/ImageCard/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.css";
 
 function ImageCard(props) {
+  console.log('process.env.PUBLIC_URL', process.env.PUBLIC_URL)
   return (
     <div className="card-game" value={props.id} onClick={() => props.handleClick(props.id)}>
       <div className="img-container">
