15a58009da373b51e46876e3ada66bf99d0cb32b
Kristen Stroup <kristenwhitson@gmail.com>
2019-11-16 21:04:50 -0600

    fix static files  for server  to  launch heroku

--- routes/api/index.js
+++ routes/api/index.js
@@ -8,9 +8,4 @@ router.use("/login", logInRoutes);
 router.use("/user", userRoutes);
 router.use("/score", scoreRoutes);
 
-// render the html page
-router.use(function(req, res){
-    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
-});
-
 module.exports = router;
